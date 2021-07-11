7c859691d8231f18ca87954d5654167c0bdf9bc7
Bob Pakhriev <boburpakhriev@gmail.com>
2021-07-11 18:03:40 +0900

    add image

--- src/index.js
+++ src/index.js
@@ -38,6 +38,15 @@ const header = (
   </header>
 );
 
+const user = (
+  <div>
+    <img
+      className="user-image"
+      src="https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-0/p526x296/129722262_1282555542109238_207706672128706420_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=e3f864&_nc_ohc=ccIdkBmE5NUAX-a1rEd&_nc_oc=AQlDWUZnnyFvOmpJwqFUKaNpJpwJ6TNL4uhNCcUvWNP8O3YVDhzZgU0eyQhKQdga828&_nc_ht=scontent-ssn1-1.xx&oh=e05853ad9e140ac964a63a5a6d5ae2a4&oe=60EEB9D6"
+      alt="bob's image"
+    />
+  </div>
+);
 const yearBorn = 1996;
 const currentYear = new Date().getFullYear();
 const age = currentYear - yearBorn;
@@ -84,6 +93,7 @@ const app = (
     {header}
     {main}
     {footer}
+    {user}
   </div>
 );
 
